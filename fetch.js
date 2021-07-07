2f8f6d2f1c6073b2051ac71c4b6bd4ff63522505
mssdfg0708 <mssdfg0708@naver.com>
2021-07-07 14:10:42 +0900

    Edit wth hashbang

--- fetch.js
+++ fetch.js
@@ -1,3 +1,30 @@
+function init(){
+    if(window.location.hash){
+        console.log("hash exist");
+        loadfile(window.location.hash.substr(2));
+      } 
+      else {
+        loadfile('welcome');
+      }
+  
+      fetch('list').then(function(response){
+      response.text().then(function(text){
+        // document.querySelector('#nav').innerHTML = text;
+        var items = text.split(',');
+        var i = 0;
+        var tags = '';
+        while(i<items.length){
+          var item = items[i];
+          item = item.trim();
+          var tag = '<li><a href="#!'+item+'" onclick="loadfile(\''+item+'\')">'+item+'</a></li>';
+          tags = tags + tag;
+          i = i + 1;
+        }
+        document.querySelector('#nav').innerHTML = tags;
+      })
+    });
+}
+
 function loadfile(file){
     fetch(file).then(function(response){
         response.text().then(function(text){
